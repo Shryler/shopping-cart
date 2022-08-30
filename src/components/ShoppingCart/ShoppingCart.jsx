9e64a87cc17673369cc69fd4d9f9e780e60c342c
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-30 09:22:47 +0200

    ok

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,15 +1,26 @@
 import "./shoppingCart.css";
 import { RiShoppingCart2Fill, RiShoppingCart2Line } from "react-icons/ri";
 
-const ShoppingCart = () => {
+const ShoppingCart = ({ itemsCount, price }) => {
 
+  // let shoppingCartIcon = <RiShoppingCart2Fill />;
+  // if (itemsCount === 0) {
+  //   shoppingCartIcon = <RiShoppingCart2Line />;
+  // }
 
-    
   return (
-    <div className="shopping-cart">
-      <RiShoppingCart2Line />
-      <span className="badge text-bg-info ms-1">0</span>
-      <span className="price-indicator ms-1">0€00</span>
+    <div className="shopping-cart d-flex align-items-center">
+      {/* Afficher le panier plein ou vide en fonction du nombre d'articles dans le panier */}
+      {/* {itemsCount > 0 && <RiShoppingCart2Fill />}
+    {itemsCount === 0 && <RiShoppingCart2Line />} */}
+      {/* OU */}
+      <span className="text-primary ">
+        {itemsCount === 0 ? <RiShoppingCart2Line size={22}/> : <RiShoppingCart2Fill size={22}/>}
+      </span>
+      {/* OU */}
+      {/* {shoppingCartIcon} */}
+      <span className="badge text-bg-info ms-1">{itemsCount}</span>
+      <span className="price-indicator ms-1">{price}€</span>
     </div>
   );
 };
