20474bf859aa6d5757b9599be71c218fe3deb81c
Shryler <105290336+Shryler@users.noreply.github.com>
2022-08-30 10:18:55 +0200

    ok

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./productCard.css";
 
-const ProductCard = ({title, price, imageId, addToCart}) => {
+const ProductCard = ({ title, price, imageId, addToCart }) => {
 
   const handleAddToCartClick = () => {
     addToCart(price);
@@ -13,15 +13,17 @@ const ProductCard = ({title, price, imageId, addToCart}) => {
   return (
     <>
       <div className="card">
-        <img src={"https://picsum.photos/id/"+imageId+"/600/400"} 
-             className="card-img-top" alt={title} />
+        <img src={"https://picsum.photos/id/" + imageId + "/600/400"}
+          className="card-img-top" alt={title} />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">
             {price}€
           </p>
-          <button className="btn btn-primary" onClick={handleAddToCartClick}>+</button>
-          <span className="ms-1"><b>{counterItem}</b> {counterItem >= 1 ? "produits" : "produit"}</span>
+          <div className="d-flex align-items-center justify-content-between">
+            <button className="btn btn-primary" onClick={handleAddToCartClick}>+</button>
+            <span><b>{counterItem}</b> {counterItem >= 1 ? "produits" : "produit"}</span>
+          </div>
         </div>
       </div>
     </>
